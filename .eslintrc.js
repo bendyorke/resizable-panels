9da911f61a9be7b8d0633655ed4f6a0e4f7de8a4
Ben Yorke <bendyorke@arya.local>
2020-03-30 10:31:03 +0200

    Update prettier and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'prettier/@typescript-eslint',
+
+    // Make sure this rule comes last
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+}
---
+++ src/ResizablePanels.tsx
@@ -0,0 +1,10 @@
+import React, {FunctionComponent} from 'react'
+import './ResizablePanels.css'
+
+type ResizablePanelsProps = {}
+
+const ResizablePanels: FunctionComponent<ResizablePanelsProps> = () => {
+  return <div className="ResizablePanels">ResizablePanels</div>
+}
+
+export default ResizablePanels
